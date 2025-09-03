da6fbdf140cd61e77949403fe8748ce12224c6b8
Ali Hussain Arafat <alihma188@gmail.com>
2025-09-03 16:01:35 +0300

    Add JUnit tests for YouTube and Google homepage validations

---
+++ src/test/java/tests/C05JunitTests01.java
@@ -0,0 +1,58 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class C05JunitTests01 {
+
+    /* Go to YouTube homepage
+    Do the following tasks by creating 4 different test methods.
+    Print "Test is running" before each test() method
+    Print "Test finished" after each test() method
+    Test if the currentURL contains "youtube"
+    Test if the title does not contain "Video".
+    Test if the URL contains "youtube".
+    Test if the sourcePage contains "youtube".
+    */
+
+   static WebDriver driver ;
+    @Test
+    void urlTest01() {
+        String currentUrl = driver.getCurrentUrl();
+        Assertions.assertEquals("https://www.youtube.com/", currentUrl);
+    }
+
+    @Test
+    void titleTest01() {
+        String title = driver.getTitle();
+        Assertions.assertFalse(title.contains("Video"));
+
+    }
+
+    @Test
+    void urlTest02() {
+        String currentUrl = driver.getCurrentUrl();
+        Assertions.assertTrue(currentUrl.contains("youtube"));
+
+    }
+    @Test
+    void pageSourceTest01() {
+        String pageSource = driver.getPageSource();
+        Assertions.assertTrue(pageSource.contains("youtube"));
+
+    }
+
+    @BeforeAll
+    static void start() {
+        driver = new ChromeDriver();
+        driver.navigate().to("https://www.youtube.com");
+        System.out.println("Test is running");
+    }
+    @AfterAll
+    static void finish() {
+        driver.quit();
+        System.out.println("Test finished");
+    }
+}
---
+++ src/test/java/tests/C06JunitTets02.java
@@ -0,0 +1,63 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C06JunitTets02 {
+
+    /* Go to google homepage
+    Do the following tasks by creating 3 different test methods.
+    Print "Tests are starting to run" before each test() method
+    Print "Tests have finished running" after each test() method
+    Test if the title is the same when the page window is maximize and minimize
+    Test if the title does not contain "Video" when the page window is fullscreen
+    Test if the URL contains "google"
+    */
+
+    WebDriver driver;
+    String url = "https://www.google.com";
+
+    @Test
+    void Test01(){
+        String titleMax;
+        String titleMin;
+        driver.navigate().to(url);
+        driver.manage().window().maximize();
+        titleMax = driver.getTitle();
+        driver.manage().window().minimize();
+        titleMin = driver.getTitle();
+        Assertions.assertEquals(titleMax, titleMin);
+
+    }
+    @Test
+    void Test02(){
+        driver.manage().window().fullscreen();
+        String titleFull = driver.getTitle();
+        Assertions.assertFalse(titleFull.contains("Video"));
+
+    }
+    @Test
+    void Test03(){
+        String currentUrl = driver.getCurrentUrl();
+        Assertions.assertTrue(currentUrl.contains("google"));
+
+    }
+
+    @BeforeEach
+    void start() {
+        driver = new ChromeDriver();
+        System.out.println("Tests are starting to run");
+        driver.navigate().to(url);
+    }
+
+    @AfterEach
+    void finish() {
+        driver.quit();
+        System.out.println("Tests have finished running");
+    }
+
+}
